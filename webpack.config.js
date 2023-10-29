99a4fd6c91be5c252f5b814462e27e643c0bb6b6
Danya <daniilka003@gmail.com>
2023-10-29 17:04:07 +0200

    test

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
     output: {
         path: path.join(__dirname, "/dist"),
         filename: "bundle.js",
+        publicPath: "https://daniilievtukhov.github.io/backendless-app",
     },
     plugins: [
         new HTMLWebpackPlugin({
